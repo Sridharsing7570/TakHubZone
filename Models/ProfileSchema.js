ed0e09d35ac4252e7525497916d7fceea45528f0
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-02-02 23:41:41 +0530

    feat: add profile schema

---
+++ Models/ProfileSchema.js
@@ -0,0 +1,27 @@
+const { model, Schema } = require("mongoose");
+
+const profileSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    skills: [String],
+    workExperience: [String],
+    certifications: [String],
+    portfolio: [String],
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+  },
+  { timestamps: true }
+);
+
+const Profile = model(profileSchema);
+module.exports = Profile;

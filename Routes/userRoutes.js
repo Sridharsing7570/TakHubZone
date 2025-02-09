c3253401742fb55d714bdbb8d95892ccd4aaf322
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-02-09 12:39:07 +0530

    feat: write code in user routes

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const userController = require("../Controllers/userController");
 
 router.post("/register", userController.registerUser);
-router.post('/login',)
-router.get('/profile/:id')
-router.put('/profile/:id')
-router.delete("/:id")
-router.get("")
+router.post("/login", userController.loginUser);
+router.get("/profile/:id", userController.getUserProfile);
+router.put("/profile/:id", userController.updateUserProfile);
+router.delete("/:id", userController.deleteUser);
+router.get("/", userController.getAllUsers);
 
 module.exports = router;

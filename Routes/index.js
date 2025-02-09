ef06ed4023adaf4d18367f5aea2e80fcf772a654
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-02-09 12:42:37 +0530

    feat:change code in index.js file in routes dir

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/", require("./userRoutes"));
+router.use("/user", require("./userRoutes"));
 
 module.exports = router;

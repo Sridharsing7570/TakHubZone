ef20d6b7a5916970dfd2c251c5beb07c67b75687
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-02-09 12:21:01 +0530

    feat:define routes for user and write two controller functions

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../Models/UserSchema");
+const secretKey = process.env.JWTsecret;
 
 // register user
 exports.registerUser = async (req, res) => {
@@ -23,5 +25,63 @@ exports.registerUser = async (req, res) => {
     res
       .status(500)
       .json({ message: "Internal Server error", error: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, userType: user.userType },
+      secretKey,
+      { expiresIn: "1d" }
+    );
+
+    res
+      .status(200)
+      .json({ message: "Successfully login", token: token, user: user });
+  } catch (error) {
+    res.status(500).json({ error: error, message: error.message });
+  }
+};
+
+// Get User Profile
+exports.getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User fetched", user });
+  } catch (error) {
+    console.error("Error during get user profile");
+    res.status(500).json({ message: "Internal server error", error: error });
+  }
+};
+
+// Update User Profile
+exports.updateUserProfile = async (req, res) => {
+  try {
+    const updateData = { ...req.body };
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      { new: true }
+    );
+
+    if (!updatedUser)
+      return res.status(404).json({ message: "User not found" });
+
+    return res
+      .status(200)
+      .json({ message: "User update successfully", user: updatedUser });
+  } catch (error) {
+    console.error("error during update user", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error });
   }
 };
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const userController = require("../Controllers/userController");
 
 router.post("/register", userController.registerUser);
+router.post('/login',)
+router.get('/profile/:id')
+router.put('/profile/:id')
+router.delete("/:id")
+router.get("")
 
 module.exports = router;

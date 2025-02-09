38d4ea46d9997eddb6d116aa3a1c556f2a69f3a5
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-02-09 12:35:30 +0530

    feat:write controller function for get all users

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -104,3 +104,17 @@ exports.deleteUser = async (req, res) => {
       .json({ message: "Internal server error", error: error });
   }
 };
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) return res.status(404).json({ message: "There is no user" });
+
+    return res.status(200).json({ message: "User found successfully" });
+  } catch (error) {
+    console.error("error during fetch user", error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server error", error: error });
+  }
+};

ef4ad0d5381f751e69e77ba54f531fed52fc7da9
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-03-09 14:40:01 +0530

    new: write a handlerFunction to fetched data

--- Controllers/jobsController.js
+++ Controllers/jobsController.js
@@ -39,3 +39,28 @@ exports.createJobs = async (req, res) => {
       .json({ message: "Internal Server error", error: error });
   }
 };
+
+exports.getJobs = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const allJobs = await Job.find().skip(skip).limit(limit).select("-__V");
+
+    const totalJobs = await Job.countDocuments();
+
+    return res.status(200).json({
+      message: "Fetched jobs successfully",
+      currentPage: page,
+      totalPages: Math.ceil(totalJobs / limit),
+      totalJobs,
+      jobs: allJobs,
+    });
+  } catch (error) {
+    logger.error(`${error} during fetching all jobs`);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error. Please try again later." });
+  }
+};

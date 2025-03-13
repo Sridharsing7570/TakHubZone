7c9e50fcbb2da684c2ad06770a5c5927fe2e3144
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-03-13 23:12:41 +0530

    new: write controller function to handle send mesage and get message

---
+++ Controllers/messageController.js
@@ -0,0 +1,90 @@
+const User = require("../Models/UserSchema");
+const logger = require("../Config/logger");
+const Message = require("../Models/MessageSchema");
+
+exports.handleSendmessage = async (req, res) => {
+  try {
+    const { senderId, receiverId, content } = req.body;
+
+    if (!senderId || !receiverId || !content) {
+      return res
+        .status(400)
+        .json({ message: "Please provided all required fields" });
+    }
+
+    // Check if the senderId and receiverId are the same
+    if (senderId === receiverId) {
+      return res
+        .status(400)
+        .json({ message: "You can't send a message to yourself" });
+    }
+    // Check if the senderId and receiverId are valid
+
+    const [sender, receiver] = await Promise.all([
+      User.findById(senderId),
+      User.findById(receiverId),
+    ]);
+
+    if (!sender || !receiver) {
+      return res
+        .status(400)
+        .json({ message: "Invalid senderId or receiverId" });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      content,
+    });
+
+    await newMessage.save();
+
+    return res.status(201).json({ message: "Message sent successfully" });
+  } catch (error) {
+    logger.error(`${error} during sending message`);
+    return res.status(500).json({ message: "Please try again later" });
+  }
+};
+
+exports.handleGetMessages = async (req, res) => {
+  try {
+    const { senderId, receiverId } = req.body;
+
+    if (!senderId || !receiverId) {
+      return res
+        .status(400)
+        .json({ message: "Please provide all required fields" });
+    }
+
+    // Check if the senderId and receiverId are the same
+    if (senderId === receiverId) {
+      return res
+        .status(400)
+        .json({ message: "You can't get messages from yourself" });
+    }
+
+    // Check if the senderId and receiverId are valid
+    const [sender, receiver] = await Promise.all([
+      User.findById(senderId),
+      User.findById(receiverId),
+    ]);
+
+    if (!sender || !receiver) {
+      return res
+        .status(400)
+        .json({ message: "Invalid senderId or receiverId" });
+    }
+
+    const messages = await Message.find({
+      $or: [
+        { senderId, receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    });
+
+    return res.status(200).json({ messages });
+  } catch (error) {
+    logger.error(`${error} during getting messages`);
+    return res.status(500).json({ message: "Please try again later" });
+  }
+};

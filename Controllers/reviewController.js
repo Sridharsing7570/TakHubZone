18c4942fd04d16c9af81ce4ca8131ace87d48214
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-03-16 00:55:13 +0530

    add: create a handler function to get review

--- Controllers/reviewController.js
+++ Controllers/reviewController.js
@@ -18,6 +18,28 @@ exports.submitReview = async (req, res) => {
       .json({ success: true, message: "Review submitted successfully" });
   } catch (error) {
     logger.error(`${error} get during create review`);
+    return res.status(500).json({
+      message: "Internal server error. Please try again later",
+      error,
+    });
+  }
+};
+
+exports.getReviewByuserId = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const review = await Review.find({ reviewedUserId: userId }).populate(
+      "reviewerId",
+      "name"
+    );
+
+    if (!review) {
+      return res.status(404).json({ message: "Review not found" });
+    }
+
+    return res.status(200).json({ success: true, review });
+  } catch (error) {
+    logger.error(`${error} during get review`);
     return res
       .status(500)
       .json({

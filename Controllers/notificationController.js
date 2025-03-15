e6a32f6583c5cea57b4c7f8e5a6cdfccdd12c09d
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-03-15 22:56:51 +0530

    add: write a notification controller function to get notifications

--- Controllers/notificationController.js
+++ Controllers/notificationController.js
@@ -6,7 +6,7 @@ const logger = require("../Config/logger");
 exports.createNotification = async (req, res) => {
   const { userId, type, message } = req.body;
   try {
-    const notification = new Notification({ userId, type, message });
+    const notification = new Notification({ userId: userId, type, message });
     await notification.save();
 
     return res.status(201).json({
@@ -19,5 +19,24 @@ exports.createNotification = async (req, res) => {
     return res
       .status(500)
       .json({ message: "Internal server error. Please try again later." });
+  }
+};
+
+// Read notifications
+exports.getNotifications = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const notifications = await Notification.find({ userId: userId }).sort({
+      createdAt: -1,
+    });
+
+    return res.status(200).json({ success: true, notifications });
+  } catch (error) {
+    logger.error(`${error} during get notifications`);
+    return res.status(500).json({
+      success: false,
+      message: `Internal server error. Please try again later`,
+      error,
+    });
   }
 };

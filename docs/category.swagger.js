dab7929a419b9c22a1480dd695d352f8c6fb0de6
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-03-16 14:40:09 +0530

    add: add swagger docs for category

---
+++ docs/category.swagger.js
@@ -0,0 +1,93 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Category
+ *   description: Category management APIs
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Category:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated ID of the category
+ *         name:
+ *           type: string
+ *           description: Name of the category
+ *         description:
+ *           type: string
+ *           description: Description of the category
+ */
+
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     summary: Create a new category
+ *     tags: [Category]
+ *     description: Add a new job category.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Category'
+ *     responses:
+ *       201:
+ *         description: Category created successfully
+ */
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     summary: Get all categories
+ *     tags: [Category]
+ *     description: Fetch all job categories.
+ *     responses:
+ *       200:
+ *         description: Successfully fetched categories
+ */
+/**
+ * @swagger
+ * /categories/{id}:
+ *   put:
+ *     summary: Update a category
+ *     tags: [Category]
+ *     description: Modify an existing job category.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID of the category to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Category'
+ *     responses:
+ *       200:
+ *         description: Category updated successfully
+ */
+/**
+ * @swagger
+ * /categories/{id}:
+ *   delete:
+ *     summary: Delete a category
+ *     tags: [Category]
+ *     description: Remove an unused category.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID of the category to delete
+ *     responses:
+ *       200:
+ *         description: Category deleted successfully
+ */

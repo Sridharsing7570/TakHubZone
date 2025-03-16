ba9230b3eb7cb4a47d41031e47e29cc3a97845a0
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-03-16 13:59:06 +0530

    add: write a controller function to handle deleting category

--- Controllers/categoryController.js
+++ Controllers/categoryController.js
@@ -55,11 +55,30 @@ exports.upddateCategories = async (req, res) => {
       .json({ success: true, message: "Category updated successfully" });
   } catch (error) {
     logger.error(`${error} during updating category`);
+    return res.status(500).json({
+      message: "Internal server error. Please try again later",
+      error,
+    });
+  }
+};
+
+// Delete category
+exports.deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedCategory = await CategorySchema.findByIdAndDelete(id);
+    if (!deletedCategory) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Category deleted successfully" });
+    }
+
     return res
-      .status(500)
-      .json({
-        message: "Internal server error. Please try again later",
-        error,
-      });
+      .status(200)
+      .json({ success: true, message: "Category deleted successfully" });
+  } catch (error) {
+    logger.error(`${error} during deleting category`);
+    return res.status(500).json({ message: "Please try again later", error });
   }
 };

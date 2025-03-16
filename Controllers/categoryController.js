8d5f19afa3773d6ae0f6aaf77ba197f2689be458
Sridharsing7570 <156458108+Sridharsing7570@users.noreply.github.com>
2025-03-16 13:45:58 +0530

    add: write a controller funtion to handle read categories

--- Controllers/categoryController.js
+++ Controllers/categoryController.js
@@ -13,11 +13,25 @@ exports.createCategory = async (req, res) => {
       .json({ success: true, message: "Category created successfully." });
   } catch (error) {
     logger.error(`${error} get during creating category`);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error. Please try again later.",
+    });
+  }
+};
+
+// Read categories
+exports.getCategories = async (req, res) => {
+  try {
+    const categories = await CategorySchema.find();
+    if (!categories) {
+      return res.status(404).json({ message: "Category not found." });
+    }
+    return res.status(200).json({ success: true, data: categories });
+  } catch (error) {
+    logger.error(`${error} during read categories`);
     return res
       .status(500)
-      .json({
-        success: false,
-        message: "Internal server error. Please try again later.",
-      });
+      .json({ message: "Internal server error. Please try again later" });
   }
 };
